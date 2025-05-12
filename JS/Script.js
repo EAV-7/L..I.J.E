0a0e502bc2d93f584cd35ceb8a25fa4b830110be
Julius Sudvojus <julius.sudvojus@stud.techin.lt>
2025-05-12 13:32:56 +0300

    burger

--- JS/Script.js
+++ JS/Script.js
@@ -1,4 +1,4 @@
-  document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.getElementById("hamburger");
     const mobileMenu = document.getElementById("mobileMenu");
     const closeBtn = document.getElementById("closeBtn");
@@ -16,14 +16,6 @@
     hamburger.addEventListener("click", openMenu);
     closeBtn.addEventListener("click", closeMenu);
 
-    // Close menu if clicking outside
-    window.addEventListener("click", function (e) {
-      if (!mobileMenu.contains(e.target) && !hamburger.contains(e.target)) {
-        closeMenu();
-      }
-    });
-
-    // Close menu on resize if width > 768px
     window.addEventListener("resize", function () {
       if (window.innerWidth > 767) {
         closeMenu();
