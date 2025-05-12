41c9dfd5b0bcae40c460b461c9912a52a7e54372
Julius Sudvojus <julius.sudvojus@stud.techin.lt>
2025-05-12 13:41:27 +0300

    burb

--- JS/Script.js
+++ JS/Script.js
@@ -1,24 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.getElementById("hamburger");
-    const mobileMenu = document.getElementById("mobileMenu");
-    const closeBtn = document.getElementById("closeBtn");
+document.getElementById("hamburger").onclick = () => {
+    document.getElementById("mobileMenu").classList.add("open");
+    document.getElementById("hamburger").style.display = "none";
+  };
 
-    function openMenu() {
-      mobileMenu.classList.add("open");
-      hamburger.style.display = "none";
-    }
-
-    function closeMenu() {
-      mobileMenu.classList.remove("open");
-      hamburger.style.display = "block";
-    }
-
-    hamburger.addEventListener("click", openMenu);
-    closeBtn.addEventListener("click", closeMenu);
-
-    window.addEventListener("resize", function () {
-      if (window.innerWidth > 767) {
-        closeMenu();
-      }
-    });
-  });
+  document.getElementById("closeBtn").onclick = () => {
+    document.getElementById("mobileMenu").classList.remove("open");
+    document.getElementById("hamburger").style.display = "block";
+  };
